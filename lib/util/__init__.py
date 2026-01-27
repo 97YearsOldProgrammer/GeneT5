a102eaef2ae2aa06542be9dd502fb605ac26f122
97YearsOldProgrammer <dsbchen@ucdavis.edu>
2026-01-26 23:33:41 -0800

    up

--- lib/util/__init__.py
+++ lib/util/__init__.py
@@ -6,7 +6,6 @@ from lib.util._databaker import (
     run_parse_data,
     process_species,
     run_tokenizer_expansion,
-    print_run_stats,
     build_species_list,
     collect_species_stats,
     write_bake_summary,
